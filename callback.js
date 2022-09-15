69dc63f98e914bddb81e954fbd4fc94046f55d3c
Anton Vasilyev <vasilyev1812@gmail.com>
2022-09-15 20:55:20 +0300

    задание 4-2

---
+++ callback.js
@@ -0,0 +1,25 @@
+const rl = require('node:readline').createInterface(process.stdin, process.stdout);
+let number = Math.floor(Math.random() * 1000),
+    count = 0;
+console.log(number);
+
+function guessNumber () {
+    rl.question('Игра "Угадай число". Нажмите "q" для выхода или введите число от 1 до 999: ', (guessUser) => {
+        if (guessUser === 'q') {
+            rl.close(console.log('Выход из игры.'));
+            return;
+        } else if (isNaN(guessUser) || (guessUser < 0 || guessUser > 999)) {
+            console.log(`Некорректное значение! Попытка ${++count}.`);
+        } else if (guessUser > number) {
+            console.log (`Больше! Попытка ${++count}.`);
+        } else if (guessUser < number) {
+            console.log (`Меньше! Попытка ${++count}.`);
+        } else {
+            console.log (`Вы угадали с ${++count} попытки.`); 
+            rl.close();
+            return;
+        } 
+        guessNumber();
+    })
+}
+guessNumber();
