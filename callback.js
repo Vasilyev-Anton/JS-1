ee3aafdcc0d85eb9d82c9c5a16b7abebc04c9cad
Anton Vasilyev <vasilyev1812@gmail.com>
2022-09-16 09:26:42 +0300

    задание 4-2

--- callback.js
+++ callback.js
@@ -6,7 +6,7 @@ console.log(number);
 function guessNumber () {
     rl.question('Игра "Угадай число". Нажмите "q" для выхода или введите число от 1 до 999: ', (guessUser) => {
         if (guessUser === 'q') {
-            return (rl.close(console.log('Выход из игры.')));
+            return (rl.close(console.log(`Выход из игры. Всего попыток было: ${count}`)));
         } else if (isNaN(guessUser) || (guessUser < 0 || guessUser > 999)) {
             console.log(`Некорректное значение! Попытка ${++count}.`);
         } else if (guessUser > number) {
