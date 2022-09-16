6e22bc9ef83080aeccdeb6ae98493959746b634d
Anton Vasilyev <vasilyev1812@gmail.com>
2022-09-16 11:14:58 +0300

    задание 4-2

--- callback.js
+++ callback.js
@@ -1,20 +1,20 @@
 const rl = require('node:readline').createInterface(process.stdin, process.stdout);
 let number = Math.floor(Math.random() * 1000),
     count = 0;
-console.log(number);
+console.log('Игра "Угадай число". Нажмите "q" для выхода или введите число от 1 до 999: ');
 
 function guessNumber () {
-    rl.question('Игра "Угадай число". Нажмите "q" для выхода или введите число от 1 до 999: ', (guessUser) => {
+    rl.question('Вы ввели: ', (guessUser) => {
         if (guessUser === 'q') {
             return (rl.close(console.log(`Выход из игры. Всего попыток: ${count}`)));
         } else if (isNaN(guessUser) || (guessUser < 0 || guessUser > 999)) {
             console.log(`Некорректное значение! Попытка ${++count}.`);
         } else if (guessUser > number) {
-            console.log (`Больше! Попытка ${++count}.`);
+            console.log (`Загаданное число меньше! Попытка ${++count}.`);
         } else if (guessUser < number) {
-            console.log (`Меньше! Попытка ${++count}.`);
+            console.log (`Загаданное число больше! Попытка ${++count}.`);
         } else {
-            return (rl.close(console.log (`Вы угадали с ${++count} попытки.`)));
+            return (rl.close(console.log (`Бинго! Загаданное число: ${number}. Вы угадали с ${++count} попытки.`)));
         } 
         guessNumber();
     })
